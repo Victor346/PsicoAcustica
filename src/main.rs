2e4bfb6ed917eba3edbbe6c5d27a4043a63c3359
Yair Pimentel <42229163+Yair0@users.noreply.github.com>
2020-11-15 14:24:25 -0600

    [UPDATE] HTML files

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,34 @@ use actix_web::{get, App, HttpServer, Result};
 async fn hello() -> Result<NamedFile> {
     Ok(NamedFile::open("./static/index.html")?)
 }
+
+#[get("/credits")]
+async fn credits() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/credits.html")?)
+}
+
+#[get("/info01")]
+async fn info01() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/infographics/info01.html")?)
+}
+#[get("/info02")]
+async fn info02() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/infographics/info02.html")?)
+}
+#[get("/info03")]
+async fn info03() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/infographics/info03.html")?)
+}
+#[get("/info04")]
+async fn info04() -> Result<NamedFile> {
+    Ok(NamedFile::open("./static/infographics/info04.html")?)
+}
+
 #[get("/game")]
 async fn game() -> Result<NamedFile> {
     Ok(NamedFile::open("./static/game.html")?)
 }
+
 
 #[get("/favicon.ico")]
 async fn favicon() -> Result<NamedFile> {
@@ -23,6 +47,11 @@ async fn main() -> std::io::Result<()> {
             .service(game)
             .service(fs::Files::new("/game", "./static/game"))
             .service(hello)
+            .service(credits)
+            .service(info01)
+            .service(info02)
+            .service(info03)
+            .service(info04)
             .service(favicon)
     })
         .bind("127.0.0.1:8080")?
